d1a70634123e32866a5775463067f967f5ab2437
amitrakshit <amit.rakshit@byjus.com>
2021-05-24 09:50:30 +0530

    Balancing the equation new files added

---
+++ Balancing_the_Equation_Hard/scripts/LO_script.js
@@ -0,0 +1,61 @@
+// SFX filrs
+var buttonAudio_Click = new Audio();
+buttonAudio_Click.src = "sounds/buton_Click.mp3";
+
+
+/* Following code till circle_stroke_animation() is for loaded screen animation */
+
+var round_stroke = document.getElementsByClassName('round_stroke')[0];
+var circle_stroke = round_stroke.children[0];
+function draw_svgArc(svgElement, cx = 0, cy = 0, radius = 1, startAngle = 0, stopAngle = 360) {
+    var xStart = radius * Math.cos(startAngle % 360 * (Math.PI / 180));
+    var yStart = radius * Math.sin(startAngle % 360 * (Math.PI) / 180);
+    var xStop = radius * Math.cos(stopAngle % 360 * (Math.PI / 180));
+    var yStop = radius * Math.sin(stopAngle % 360 * (Math.PI) / 180);
+    if (stopAngle - startAngle <= 180) {
+        var large_arc_sweep_flag = ' 0 0 1 ';
+    }
+    else {
+        var large_arc_sweep_flag = ' 0 1 1 ';
+    }
+    /* The formate of svg ard is <path d="MstartX YstartY ARadiusX RadiusY 0 0 0 endX endY" ></path>*/
+    var stringAttribute = "M" + (cx + xStart).toString() + ' ' + (cy + yStart).toString() +
+        ' A' + radius.toString() + " " + radius.toString() + large_arc_sweep_flag
+        + (cx + xStop).toString() + " " + (cy + yStop).toString();
+    svgElement.setAttribute('d', stringAttribute);
+}
+
+var start_Angle = -90;
+function circle_stroke_animation() {
+    var circle_animation = setInterval(function () {
+        if (start_Angle <= 270) {
+            draw_svgArc(circle_stroke, 100, 100, 89, start_Angle, 270);
+            start_Angle += 3;
+        }
+        else {
+            clearInterval(circle_animation);
+        }
+    }, 1000/120);
+
+}
+
+
+
+// LO messagig screen
+var LO_message = document.getElementsByClassName('LO_message')[0];
+var popup = document.getElementsByClassName('popup')[0];
+var close_button = document.getElementsByClassName('close_button')[0];
+
+close_button.onclick = function () {
+    buttonAudio_Click.play();
+    LO_message.style.backgroundColor = '#00000000';
+    popup.style = 'opacity:0; top: 950px'
+    LO_message.style = "z-index : -1;";
+}
+
+
+//Finish Button
+var finish_button = document.getElementsByClassName('finish-button')[0];
+finish_button.onclick = function () {
+   window.location.reload();
+}
--- Balancing_the_Equation_Hard/scripts/index_script.js
+++ Balancing_the_Equation_Hard/scripts/index_script.js
@@ -5,18 +5,33 @@ var metaData = document.getElementsByName('viewport');
 metaData[0].content = "width=device-width, initial-scale=" + 1 / screenScaleRatio.toString();
 //console.log(metaData[0].content);
 var loading_screen = document.getElementById('loading-screen');
- 
+
 // window.onresize = function () {
 //     location.reload();
 // }
 /* For filling up the numbers which will slide */
- 
+
+
+// Following code is for question tab and observation tab transition
+var navigation_bar = document.getElementsByClassName('navigation_bar');
+var question_tab = navigation_bar[0].children[1];
+var observation_tab = navigation_bar[1].children[0];
+var movable_parent = document.getElementsByClassName('movable_parent')[0];
+question_tab.onclick = function () {
+   movable_parent.style = "left: -1080px;";
+}
+
+observation_tab.onclick = function () {
+   movable_parent.style = "left: 0px";
+}
+
+
 var number_container = document.getElementsByClassName('numbers_container');
- 
+
 for (let elements of number_container) {
-    var random = Math.floor(Math.random() * 8 + 1);
-    elements.style.top = (-random * 100).toString() + '%';
-    for (let j = 0; j < 9; j++) {
+    // var random = Math.floor(Math.random() * 3 + 1);
+    elements.style.top = (-0 * 100).toString() + '%';
+    for (let j = 0; j < 4; j++) {
         var temp = document.createElement('span');
         temp.className = 'inside_number';
         temp.innerText = (j + 1);
@@ -27,114 +42,141 @@ for (let elements of number_container) {
 var check_1 = document.getElementById('first_check');
 var check_2 = document.getElementById('second_check');
 var check_3 = document.getElementById('third_check');
- 
- 
+var remarks = document.getElementById("para");
+var btn1 = document.getElementById("button1");
+var btn2 = document.getElementById("button2");
+var btn3 = document.getElementById("button3");
+var btn4 = document.getElementById("button4");
+var btn5 = document.getElementById("button5");
+var btn6 = document.getElementById("button6");
+var btn7 = document.getElementById("button7");
+var btn8 = document.getElementById("button8");
+var sound = document.getElementById("button_sound");
+btn2.disabled = true;
+btn2.style.opacity = 0.4;
+btn4.disabled = true;
+btn4.style.opacity = 0.4;
+btn6.disabled = true;
+btn6.style.opacity = 0.4;
+btn8.disabled = true;
+btn8.style.opacity = 0.4;
+
+
 /*... for the transition of numbers using buttons...*/
 function poly1() {
-    if (reactant_1.multiplier < 9) {
+    if (reactant_1.multiplier < 4) {
         var top_measure = Number(number_container[0].style.top.slice(0, -1));
         top_measure = top_measure - 100;
         number_container[0].style.top = top_measure.toString() + '%';
         reactant_1.multiplier = Number(number_container[0].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
+        sound.playbackRate = 3;
         reactant_1.update_element_numbers();
         update_reactant_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly2() {
-    if (reactant_2.multiplier < 9) {
+    if (reactant_2.multiplier < 4) {
         var top_measure = Number(number_container[1].style.top.slice(0, -1));
         top_measure = top_measure - 100;
         number_container[1].style.top = top_measure.toString() + '%';
         reactant_2.multiplier = Number(number_container[1].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         reactant_2.update_element_numbers();
         update_reactant_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly3() {
-    if (product_1.multiplier < 9) {
+    if (product_1.multiplier < 4) {
         var top_measure = Number(number_container[2].style.top.slice(0, -1));
         top_measure = top_measure - 100;
         number_container[2].style.top = top_measure.toString() + '%';
         product_1.multiplier = Number(number_container[2].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         product_1.update_element_numbers();
         update_product_column();
         update_check_column();
         paragraph_show();
- 
+
     }
 }
- 
+
 function poly4() {
-    if (product_2.multiplier < 9) {
+    if (product_2.multiplier < 4) {
         var top_measure = Number(number_container[3].style.top.slice(0, -1));
         top_measure = top_measure - 100;
         number_container[3].style.top = top_measure.toString() + '%';
         product_2.multiplier = Number(number_container[3].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         product_2.update_element_numbers();
         update_product_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly5() {
     if (reactant_1.multiplier > 1) {
         var top_measure = Number(number_container[0].style.top.slice(0, -1));
         top_measure = top_measure + 100;
         number_container[0].style.top = top_measure.toString() + '%';
         reactant_1.multiplier = Number(number_container[0].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         reactant_1.update_element_numbers();
         update_reactant_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly6() {
     if (reactant_2.multiplier > 1) {
         var top_measure = Number(number_container[1].style.top.slice(0, -1));
         top_measure = top_measure + 100;
         number_container[1].style.top = top_measure.toString() + '%';
         reactant_2.multiplier = Number(number_container[1].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         reactant_2.update_element_numbers();
         update_reactant_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly7() {
     if (product_1.multiplier > 1) {
         var top_measure = Number(number_container[2].style.top.slice(0, -1));
         top_measure = top_measure + 100;
         number_container[2].style.top = top_measure.toString() + '%';
         product_1.multiplier = Number(number_container[2].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         product_1.update_element_numbers();
         update_product_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 function poly8() {
     if (product_2.multiplier > 1) {
         var top_measure = Number(number_container[3].style.top.slice(0, -1));
         top_measure = top_measure + 100;
         number_container[3].style.top = top_measure.toString() + '%';
         product_2.multiplier = Number(number_container[3].style.top.slice(0, -1)) / -100 + 1;
+        sound.play();
         product_2.update_element_numbers();
         update_product_column();
         update_check_column();
         paragraph_show();
     }
 }
- 
+
 var table_data = document.getElementsByTagName('td');
 var reactant_1 = {
     Fe: 2, O: 3, multiplier: 1,
@@ -160,10 +202,10 @@ var product_2 = {
     update_element_numbers: function () {
         this.C = this.multiplier * 1;
         this.O = this.multiplier * 2;
- 
+
     }
 };
- 
+
 //Initialising the values of table data and other
 reactant_1.multiplier = Number(number_container[0].style.top.slice(0, -1)) / -100 + 1;
 reactant_2.multiplier = Number(number_container[1].style.top.slice(0, -1)) / -100 + 1;
@@ -173,33 +215,33 @@ product_1.update_element_numbers();
 product_2.update_element_numbers();
 reactant_1.update_element_numbers();
 reactant_2.update_element_numbers();
- 
+
 var reactant_column = [];
 var product_column = [];
 var balance_column = []; 
 for (let i = 0; i < 4; i++) {
     reactant_column[i] = table_data[4 * i + 1];
-    product_column[i] = table_data[4 * i + 2];
-    balance_column[i] = table_data[4*i + 3];
+    balance_column[i] = table_data[4 * i + 2];
+    product_column[i] = table_data[4*i + 3];
 }
- 
+
 function update_reactant_column() {
     reactant_column[0].innerText = reactant_1.Fe;
     reactant_column[1].innerText = reactant_2.C;
     reactant_column[2].innerText = reactant_1.O;
 }
- 
+
 function update_product_column() {
     product_column[0].innerText = product_1.Fe;
     product_column[1].innerText = product_2.C;
     product_column[2].innerText = product_2.O;
 }
- 
+
 update_reactant_column();
 update_product_column();
 update_check_column();
-paragraph_show();
- 
+
+
 function update_check_column()
 {
 if (reactant_column[0].innerText != product_column[0].innerText)
@@ -211,7 +253,7 @@ if (reactant_column[0].innerText != product_column[0].innerText)
     check_1.classList.remove("wrong");
     check_1.classList.add("right");
 }
- 
+
 if (reactant_column[1].innerText != product_column[1].innerText){
     check_2.classList.remove("right");
     check_2.classList.add("wrong");
@@ -230,32 +272,100 @@ if (reactant_column[2].innerText != product_column[2].innerText)
     check_3.classList.add("right");
 }
 }
-/*... for showing the feedback message...*/
+
+
 function paragraph_show()
 {
-var remarks = document.getElementById("para");
-if (check_1.classList[1] == "right" && check_2.classList[1] == "right" && check_3.classList[1] == "right")
-    {   
-        if (reactant_1.multiplier == 2 && reactant_2.multiplier == 3)
-            {
-                remarks.innerText = "The equation is balanced.";
-            }
-       else 
-            {
-            remarks.innerText = "The equation is balanced but the coefficients should be smallest possible integer.";
-            }
-    }
-else
-   {
-    remarks.innerText = "The equation is not balanced yet.";
-   }
+if ( btn3.disabled == false && btn7.disabled == false)
+{ if(btn1.disabled == false && btn5.disabled == false) {iron_feedback();}
+  else{final_feedback();}
 }
- 
- 
- 
- 
- 
- 
- 
- 
+else if (btn1.disabled == false && btn4.disabled == false && btn5.disabled == false && btn8.disabled == false)
+{oxygen_feedback();}
+else if (btn2.disabled == false && btn4.disabled == false && btn6.disabled == false && btn8.disabled == false)
+{carbon_feedback();}
+}
+
+
+function iron_feedback()
+{
+var remarks = document.getElementById("para");
+if (reactant_1.multiplier == 1 && product_1.multiplier == 2)
+    {remarks.innerText = "Awesome! You have balanced iron atoms on both sides.";}
+ else if (reactant_1.multiplier == 2 && product_1.multiplier == 4)
+    {remarks.innerText = "Awesome! Iron atoms are equal but you could have gotten it done with a much lower number. Try again.";}
+    
+else{remarks.innerText = "Iron atoms are not balanced. Try making the number of iron atoms equal on both reactant and product side.";}
+}
+
+function next_instruction()
+{
+var remarks = document.getElementById("para");
+if (remarks.innerText == "Awesome! You have balanced iron atoms on both sides.")
+{ sound.play();
+remarks.innerText = "Awesome! Let us look to balance the oxygen atoms next.";
+btn3.disabled = true;
+btn3.style.opacity = 0.4;
+btn4.disabled = false;
+btn4.style.opacity = 1;
+btn7.disabled = true;
+btn7.style.opacity = 0.4;
+btn8.disabled = false;
+btn8.style.opacity = 1;
+}
+else if (remarks.innerText == "Awesome! You have balanced oxygen atoms on both sides")
+{ sound.play();
+remarks.innerText = "Awesome! Let us look to balance the carbon atoms next.";
+btn1.disabled = true; btn1.style.opacity = 0.4;
+btn2.disabled = false; btn2.style.opacity = 1;
+btn5.disabled = true; btn5.style.opacity = 0.4;
+btn6.disabled = false; btn6.style.opacity = 1;
+}
+else if(remarks.innerText == "Awesome! You have balanced carbon atoms on both sides.")
+{ sound.play();
+remarks.innerText = "Awesome! But looks like the Fe atoms are again unequal.";
+btn2.disabled = true; btn2.style.opacity = 0.4;
+btn3.disabled = false;btn3.style.opacity = 1;
+btn4.disabled = true;btn4.style.opacity = 0.4;
+btn6.disabled = true;btn6.style.opacity = 0.4;
+btn7.disabled = false;btn7.style.opacity = 1;
+btn8.disabled = true;btn8.style.opacity = 0.4;
+}
+else if (remarks.innerText == "Awesome! You have made Fe atoms equal on both sides and also the entire equation looks balanced.")
+{sound.play();
+
+    movable_parent.style = "left: -1080px;";
+}
+
+}
+
+function oxygen_feedback() 
+{
+if (reactant_1.multiplier == 2 && product_2.multiplier == 3)
+{remarks.innerText = "Awesome! You have balanced oxygen atoms on both sides";}
+else{remarks.innerText = "Oxygen atoms are not balanced. Try making the number of oxygen atoms equal on both reactant and product side";}
+}
+
+
+function carbon_feedback()
+{
+if (reactant_2.multiplier == product_2.multiplier)
+{
+    if (reactant_2.multiplier == 3)
+    {remarks.innerText = "Awesome! You have balanced carbon atoms on both sides."}
+    else{remarks.innerText = "This number has unbalanced the oxygen atoms, lets try some other number to balance the carbon atoms."}
+}
+else{remarks.innerText = "Carbon atoms are not balanced. Try making the number of carbon atoms equal on both reactant and product side."}
+}
+
+function final_feedback()
+{
+    if (reactant_1.multiplier == 2 && product_1.multiplier == 4)   
+    {remarks.innerText = "Awesome! You have made Fe atoms equal on both sides and also the entire equation looks balanced.";}
+    else{remarks.innerText = "Observe that the number of Fe atoms are unequal on both the sides.";}
+}
+
+
+
+
 
