e97a5e0fc6f6096d9c5b54466dbbfb88df2f7eb9
amitrakshit <amit.rakshit@byjus.com>
2021-06-02 21:10:15 +0530

    Minor changes to Balancing Equation Hard corrected.

--- Balancing_the_Equation_Hard_Corrected20210601/scripts/context_card.js
+++ Balancing_the_Equation_Hard_Corrected20210601/scripts/context_card.js
@@ -1,16 +1,31 @@
 var context_button = document.getElementsByClassName('context_button')[0];
 var context_card = document.getElementsByClassName('contex_card')[0];
 var context_card_closed = true;
-context_button.addEventListener('click', function(event){
-    if (context_card_closed){
+context_button.addEventListener('click', function (event) {
+    buttonAudio_Click.play();
+    if (context_card_closed) {
         context_card.style.setProperty('transform', 'scale(1)');
         context_card.style.opacity = 1;
         context_card_closed = false;
     }
-    else{
+    else {
+        buttonAudio_Click.play();
         context_card.style.setProperty('transform', 'scale(0)');
-        context_card.style = "opacity: 0.0";
+        context_card.style.opacity = 0;
         context_card_closed = false;
         context_card_closed = true;
     }
 })
+
+console.log(window.innerHeight);
+var Tobescaled = window.innerHeight / 1920;
+
+if (window.innerWidth / window.innerHeight > 9 / 16) {
+    var Tobescaled = window.innerHeight / 1920;
+    document.getElementsByClassName('window')[0].style.setProperty('transform', 'scale(' + Tobescaled.toString() + ')');
+}
+else {
+    var Tobescaled = window.innerWidth / 1080;
+    document.getElementsByClassName('window')[0].style.setProperty('transform', 'scale(' + Tobescaled.toString() + ')');
+    document.getElementsByClassName('window')[0].style = "transform-origin:center;";
+}
