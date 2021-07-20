90aae42cdeacde7aa961053b479c308ff8c12cfb
amitrakshit <amit.rakshit@byjus.com>
2021-07-20 20:11:47 +0530

    MYopia only MSQ template added...

--- Myopia_and_Hypermetropia/scripts/question_script.js
+++ Myopia_and_Hypermetropia/scripts/question_script.js
@@ -20,6 +20,10 @@ var feedback_bar = document.getElementsByClassName('feedback-bar')[0];
 //geting the options into account
 var option_container = document.getElementsByClassName("option-container")[0];
 var options = document.getElementsByClassName("options");
+var optionsChildren = document.getElementsByClassName('checkBox');
+
+// the behind divs for all options
+var behindDivs = document.getElementsByClassName("behindText");
 
 //solution and footer
 var solution = document.getElementsByClassName('solution')[0];
@@ -30,39 +34,137 @@ var solution_clicked = false;
 var which_button_clicked = 1;
 for (let index = 0; index < options.length; index++) {
     options[index].onclick = function () {
-        if (this.title == "solution" && solution_clicked == false) {
-            options[which_button_clicked - 1].style = "background: white; color: black;"
-            solution_clicked = true;
-            feedback_bar.style.opacity = "1";
-            feedback_bar.children[0].style = "color: #8DD43E;";
-            feedback_bar.children[0].innerHTML = "Well Done!";
-            feedback_bar.children[1].style = "color: #8DD43E;";
-            feedback_bar.children[1].innerHTML = "You got the correct answer!";
-            this.style = "background:linear-gradient(180deg, #3CA35A, #389B60); color: white;";
-            dynamic_panel_window.style = "overflow-y:scroll;";
-            dynamic_panel_window.scroll({ top: 800, behavior: "smooth" });
-            footer.style = "bottom:0rem";
-            Right_ANS.play();
-
-            //Lo message
-            buttonAudio_Click.play();
-            LO_message.style = "z-index : 0;";
-            LO_message.style.backgroundColor = '#000000cc';
-            popup.style = 'opacity:1; top: 601rem'
-            circle_stroke_animation();
-            setTimeout(function () {
-                close_button.style.opacity = 1;
-            }, 1000)
+        if (userResponse[index] == false) {
+            userResponse[index] = true;
+            optionsChildren[index].children[0].classList.add('checked');
         }
-        else if (this.title !== "solution" && solution_clicked == false) {
-            options[which_button_clicked - 1].style = "background: white; color: black;"
-            this.style = "background:linear-gradient(180deg, #FB6F6F, #EB5151); color: white;";
-            feedback_bar.style = "opacity:1; top: 1050rem";
-            Wrong_ANS.play();
+        else if (userResponse[index] == true) {
+            userResponse[index] = false;
+            optionsChildren[index].children[0].classList.remove('checked');
         }
-        which_button_clicked = (this.id).charAt(this.id.length - 1);
     }
 }
 
 // Finish Button
 // functionality added in LO_script
+
+
+
+// Submit button
+var submitButton = document.getElementsByClassName('submitButton')[0];
+submitButton.onclick = function () {
+    buttonAudio_Click.play();
+    submitAns();
+}
+
+var correctResponse = [true, false, true];
+var userResponse = [false, false, false];
+
+var greenGradient = "linear-gradient(180deg, #3CA35A, #389B60)";
+var redGradient = "linear-gradient(180deg, #FB6F6F, #EB5151)";
+
+function submitAns() {
+    var rightAnswerStatus = false; var wrongAnswerStatus = false;
+    for (let i = 0; i <= correctResponse.length; i++) {
+        if (userResponse[i] == true && correctResponse[i] == true) {
+            Object.assign(options[i].style, { background: greenGradient, color: "white" });
+            rightAnswerStatus = true;
+            optionsChildren[i].classList.add('answered');
+            optionsChildren[i].children[0].classList.add('rightAns');
+            options[i].onclick = () => {
+                buttonAudio_Click.play();
+            }
+        }
+        else if (userResponse[i] == true && correctResponse[i] == false) {
+            Object.assign(options[i].style, { background: redGradient, color: "white" });
+            wrongAnswerStatus = true;
+            optionsChildren[i].classList.add('answered');
+            optionsChildren[i].children[0].classList.add('wrongAns');
+        }
+        else {
+            if (correctResponse[i] == true) {
+                wrongAnswerStatus = true;
+            }
+        }
+    }
+
+    if (rightAnswerStatus == true && wrongAnswerStatus == false) {
+        // correct answer
+        Right_ANS.play();
+        Object.assign(feedback_bar.style, { opacity: 1, top: "1000rem" });
+        feedback_bar.children[0].style = "color: #8DD43E;";
+        feedback_bar.children[0].innerHTML = "Well Done!";
+        feedback_bar.children[1].style = "color: #8DD43E;";
+        feedback_bar.children[1].innerHTML = "You got the correct answer!";
+        dynamic_panel_window.style = "overflow-y:scroll;";
+        dynamic_panel_window.scroll({ top: 350, behavior: "smooth" });
+        footer.style = "bottom:0rem";
+
+        // Hiding the submit button
+        Object.assign(submitButton.style, { display: 'none' });
+
+        // LO pop up
+        LO_message.style = "z-index : 0;";
+        LO_message.style.backgroundColor = '#000000cc';
+        popup.style = 'opacity:1; top: 580rem'
+        circle_stroke_animation();
+        setTimeout(function () {
+            close_button.style.opacity = 1;
+        }, 1000);
+    }
+    else if (rightAnswerStatus == true && wrongAnswerStatus == true) {
+        // partially correct answer
+        feedback_bar.children[1].innerHTML = "You have partially answered. <br> Try again. There are total two correct answers.";
+        Object.assign(feedback_bar.children[1].style, { width: '90%' });
+        Object.assign(feedback_bar.style, { opacity: 1, top: "970rem" });
+        Wrong_ANS.play();
+        submitButton.innerHTML = 'Retry';
+        submitButton.onclick = function () {
+            buttonAudio_Click.play();
+            retry();
+        }
+    }
+    else if (rightAnswerStatus == false && wrongAnswerStatus == true) {
+        // wrong answer
+        Object.assign(feedback_bar.style, { opacity: 1, top: "970rem" });
+        Wrong_ANS.play();
+        submitButton.innerHTML = 'Retry';
+        submitButton.onclick = function () {
+            buttonAudio_Click.play();
+            retry();
+        }
+    }
+
+}
+
+function retry() {
+    for (let i = 0; i < correctResponse.length; i++) {
+        if (userResponse[i] == true && correctResponse[i] == true) {
+            Object.assign(optionsChildren[i].style, { display: 'none' });
+            options[i].onclick = function () {
+                popDownTxt(this);
+            }
+        }
+        else {
+            userResponse[i] = false;
+            optionsChildren[i].children[0].classList.remove('wrongAns');
+            optionsChildren[i].children[0].classList.remove('checked');
+            optionsChildren[i].classList.remove('answered');
+            Object.assign(options[i].style, { background: 'white', color: '#333333' });
+        }
+    }
+    submitButton.innerHTML = "Submit";
+    submitButton.onclick = function () {
+        buttonAudio_Click.play();
+        submitAns();
+    }
+}
+
+function popDownTxt(option) {
+    var index = (option.id).charAt(option.id.length - 1);
+    behindDivs[index - 1].style.setProperty('--downwardDisplacement', '30rem');
+    setTimeout(function () {
+        behindDivs[index - 1].style.setProperty('--downwardDisplacement', '0rem');
+    }, 2000);
+
+}
